0cf5eb1360750bc6717579111b07cb08666644d0
Henry Nguyen <nguyenhai1591@gmail.com>
2021-01-29 22:37:36 +1000

    Static generation with data

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,29 @@
 import Layout from '../../components/layouts/Layout'
+import Card from 'react-bootstrap/Card'
 
-const Post = () => (
+const posts = [
+  {
+    id: 1,
+    title: 'Post 1',
+    body: 'My Post 1 body'
+  },
+  {
+    id: 2,
+    title: 'Post 2',
+    body: 'My Post 2 body'
+  }
+]
+const Posts = () => (
   <Layout>
-    <h1>My Posts</h1>
+    {posts.map(post => (
+      <Card key={post.id}>
+        <Card.Body>
+          <Card.Title>{post.title}</Card.Title>
+          <Card.Text>{post.body}</Card.Text>
+        </Card.Body>
+      </Card>
+    ))}
   </Layout>
 )
 
-export default Post
+export default Posts
