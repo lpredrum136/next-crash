38aca127be136b0dfb517470ce2273c764eb87f0
Henry Nguyen <nguyenhai1591@gmail.com>
2021-01-31 20:54:18 +1000

    Global CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 // import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css' // use bootswatch,
+// download bootstrap.min.css and overwrite to change this bootstrap.min.css to get new theme
+// must restart server
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
