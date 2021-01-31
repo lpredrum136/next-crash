733d528092bd94d9d8a64dc219aa96d9235eceba
Henry Nguyen <nguyenhai1591@gmail.com>
2021-01-31 12:13:31 +1000

    getStaticPaths - fallback: true - to avoid prerendering too many pages based on paths

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,9 +2,27 @@ import Layout from '../../components/layouts/Layout'
 import { getPostById, getPostIds } from '../../lib/post'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import Spinner from 'react-bootstrap/Spinner'
+
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const Post = ({ post }) => {
+  const router = useRouter()
+
+  // If the page is not yet generated, isFallback is always `true`
+  // and this Loading... will be displayed
+  // initially until getStaticProps() finishes running (for the first time)
+  // When finished, Next adds this page to list of prerendered page (kinda cache) for any subsequent requests
+  if (router.isFallback) {
+    return (
+      <Spinner animation='border' role='status' variant='dark'>
+        <span className='sr-only'>Loading...</span>
+      </Spinner>
+    )
+  }
+
+  // Render post
   return (
     <Layout>
       <Card className='my-3 shadow'>
@@ -25,7 +43,9 @@ export const getStaticPaths = async () => {
   // console.log(paths)
   return {
     paths,
-    fallback: false // any paths not returned by getStaticPaths will result in a 404 page
+    // fallback: false // any paths not returned by getStaticPaths will result in a 404 page
+    fallback: true // show spinner first and wait for getStaticProps to finish
+    // in web, you may have to refresh the posts/<not-prerendered-id> to see the spinner
   }
 }
 
